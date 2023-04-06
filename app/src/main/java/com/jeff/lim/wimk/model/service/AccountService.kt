14d8c875c70c400838c8e854f94bb95d7a153241
wootaek.lim <wootaek.lim@42dot.ai>
2023-04-06 17:59:31 +0900

    사용자 등록 Firebase DB 연동

--- app/src/main/java/com/jeff/lim/wimk/model/service/AccountService.kt
+++ app/src/main/java/com/jeff/lim/wimk/model/service/AccountService.kt
@@ -1,13 +1,9 @@
 package com.jeff.lim.wimk.model.service
 
-import com.jeff.lim.wimk.model.User
-import kotlinx.coroutines.flow.Flow
-
 interface AccountService {
     val currentUserId: String
     val currentUserEmail: String
     val hasUser: Boolean
-    val currentUser: Flow<User>
 
     suspend fun logIn(email: String, password: String): Boolean
     suspend fun signUp(email: String, password: String)
--- app/src/main/java/com/jeff/lim/wimk/model/service/impl/AccountServiceImpl.kt
+++ app/src/main/java/com/jeff/lim/wimk/model/service/impl/AccountServiceImpl.kt
@@ -1,11 +1,7 @@
 package com.jeff.lim.wimk.model.service.impl
 
 import com.google.firebase.auth.FirebaseAuth
-import com.jeff.lim.wimk.model.User
 import com.jeff.lim.wimk.model.service.AccountService
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
 import timber.log.Timber
 import javax.inject.Inject
@@ -21,16 +17,6 @@ class AccountServiceImpl @Inject constructor(private val auth: FirebaseAuth) : A
 
     override val hasUser: Boolean
         get() = auth.currentUser != null
-
-    override val currentUser: Flow<User>
-        get() = callbackFlow {
-            val listener =
-                FirebaseAuth.AuthStateListener { auth ->
-                    this.trySend(auth.currentUser?.let { User(it.uid, it.isAnonymous) } ?: User())
-                }
-            auth.addAuthStateListener(listener)
-            awaitClose { auth.removeAuthStateListener(listener) }
-        }
 
     override suspend fun logIn(email: String, password: String): Boolean {
         var result = false
